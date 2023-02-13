b245fdd6712e17d5bad622f3f9569b9d65764ff3
us254 <88821507+us254@users.noreply.github.com>
2023-02-13 17:19:08 +0330

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,52 +1,17 @@
 #!/bin/bash
 
-_APISERVER=127.0.0.1:10085
-_XRAY=/usr/local/bin/xray
+echo "User\tUplink\tDownlink"
 
-apidata () {
-    local ARGS=
-    if [[ $1 == "reset" ]]; then
-      ARGS="-reset=true"
-    fi
-    $_XRAY api statsquery --server=$_APISERVER "${ARGS}" \
-    | awk '{
-        if (match($1, /"name":/)) {
-            f=1; gsub(/^"|link"|,$/, "", $2);
-            split($2, p,  ">>>");
-            printf "%s:%s->%s\t", p[1],p[2],p[4];
-        }
-        else if (match($1, /"value":/) && f){
-          f = 0;
-          gsub(/"/, "", $2);
-          printf "%.0f\n", $2;
-        }
-        else if (match($0, /}/) && f) { f = 0; print 0; }
-    }'
-}
+# Get a list of all users
+users=$(cut -d: -f1 /etc/passwd)
 
-print_sum() {
-    local DATA="$1"
-    local PREFIX="$2"
-    local SORTED=$(echo "$DATA" | grep "^${PREFIX}" | sort -r)
-    local SUM=$(echo "$SORTED" | awk '
-        /->up/{us+=$2}
-        /->down/{ds+=$2}
-        END{
-            printf "SUM->up:\t%.0f\nSUM->down:\t%.0f\nSUM->TOTAL:\t%.0f\n", us, ds, us+ds;
-        }')
-    echo -e "${SORTED}\n${SUM}" \
-    | numfmt --field=2 --suffix=B --to=iec \
-    | column -t
-}
-
-DATA=$(apidata $1)
-echo "------------Inbound----------"
-print_sum "$DATA" "inbound"
-echo "-----------------------------"
-echo "------------Outbound----------"
-print_sum "$DATA" "outbound"
-echo "-----------------------------"
-echo
-echo "-------------User------------"
-print_sum "$DATA" "user"
-echo "-----------------------------"
+# Iterate through each user
+for user in $users; do
+  # Check if the user has a home directory
+  if [ -d "/home/$user" ]; then
+    # Retrieve the uplink and downlink traffic for the user
+    uplink=$(sudo -u $user vnstat --json | jq '.interfaces[0].traffic.tx' | awk '{sum+=$1} END {print sum/1024/1024}')
+    downlink=$(sudo -u $user vnstat --json | jq '.interfaces[0].traffic.rx' | awk '{sum+=$1} END {print sum/1024/1024}')
+    echo "$user\t$uplink\t$downlink"
+  fi
+done
